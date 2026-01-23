634883713aed0dbfc99be31a19b5326e80ca8f4d
Gabriel Anselmo <gabriel.anselmo.98@gmail.com>
2026-01-23 17:25:00 -0300

    Service sell operation

--- src/main/java/Service/CapitalGainsService.java
+++ src/main/java/Service/CapitalGainsService.java
@@ -43,10 +43,41 @@ public class CapitalGainsService {
 
             } else {
 
-                BigDecimal sellPrice = op.
+                BigDecimal sellPrice = op.getUnitCost();
+                int sellQty = op.getQuantity();
 
+                BigDecimal totalOperationValue = sellPrice.multiply(new BigDecimal(sellQty));
+
+                BigDecimal originalCost = weightedAvarege.multiply(new BigDecimal(sellQty));
+
+                BigDecimal profitOrLoss = totalOperationValue.subtract(originalCost);
+
+                currentQuantity -= sellQty;
+
+                if (profitOrLoss.compareTo(BigDecimal.ZERO) < 0) {
+
+                    accumulatedLoss = accumulatedLoss.add(profitOrLoss.abs());
+                    results.add(new TaxResult(BigDecimal.ZERO));
+
+                } else {
+
+                    if (accumulatedLoss.compareTo(BigDecimal.ZERO) > 0) {
+                        if (accumulatedLoss.compareTo(profitOrLoss) >= 0) {
+
+                            accumulatedLoss = accumulatedLoss.subtract(profitOrLoss);
+                            profitOrLoss = BigDecimal.ZERO;
+                        } else {
+
+                            profitOrLoss = profitOrLoss.subtract(accumulatedLoss);
+                            accumulatedLoss = BigDecimal.ZERO;
+                        }
+                    }
+
+                    BigDecimal tax = profitOrLoss.multiply(new BigDecimal("0.20"));
+                    results.add(new TaxResult(tax));
+                }
             }
         }
-
+        return results;
     }
 }
